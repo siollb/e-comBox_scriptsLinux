2bc8b868e437278e97a65033f7ed9c537afa754d
aporaf <root@servecomBox.llb.fr>
2019-07-24 12:14:58 +0200

    Réorganisation des scripts

--- install_application.sh
+++ install_application.sh
@@ -6,14 +6,11 @@
 # Couleurs
 COLTITRE="\033[1;35m"   # Rose
 COLPARTIE="\033[1;34m"  # Bleu
-
 COLTXT="\033[0;37m"     # Gris
 COLCHOIX="\033[1;33m"   # Jaune
 COLDEFAUT="\033[0;33m"  # Brun-jaune
 COLSAISIE="\033[1;32m"  # Vert
-
 COLCMD="\033[1;37m"     # Blanc
-
 COLERREUR="\033[1;31m"  # Rouge
 COLINFO="\033[0;36m"    # Cyan
 
@@ -46,11 +43,11 @@ POURSUIVRE()
 	fi
 }
 
-clear
+#clear
 echo -e "$COLTITRE"
 echo "***************************************************"
-echo "*  SCRIPT PERMETTANT D'INSTALLER E-COMBOX ET DE   *"
-echo "*           CONFIGURER SON ENVIRONNEMENT          *"
+echo "*     INSTALLATION DE E-COMBOX ET CONFIGURATION   *"
+echo "*                DE SON ENVIRONNEMENT             *"
 echo "***************************************************"
 
 echo -e "$COLPARTIE"
@@ -60,25 +57,25 @@ echo ""
 
 #Gestion des adresses IP
 echo -e "$COLTXT"
-echo -e "Saisissez l'adresse IP privé du serveur: $COLSAISIE\c"
+echo -e "Saisissez l'adresse IP privée du serveur: $COLSAISIE\c"
 read ADRESSE_IP_PRIVE
 
 echo -e "$COLTXT"
-echo -e " Si le serveur doit être accessible de l'extérieur, saisissez l'adresse IP publique: $COLSAISIE\c"
-echo -e "Laissez vide si le serveur ne sera pas accessible de l'extérieur" 
+echo -e " Si le serveur doit être accessible de l'extérieur, saisissez l'adresse IP publique ou un nom de domaine pleinement qualifié. C'est cette adresse IP ou ce nom de domaine qui apparaîtra au niveau de chaque site créé : $COLSAISIE\n"
+echo -e "Laisser vide si le serveur ne sera pas accessible de l'extérieur. L'application e-comBox utilisera l'adresse IP privée." 
 read ADRESSE_IP_PUBLIQUE
 
 
 #Gestion du proxy
 echo -e "$COLTXT"
-echo -e "Saisissez l'adresse du proxy : $COLSAISIE\c"
+echo -e "Saisissez l'adresse du proxy : $COLSAISIE\n"
 echo "Laisser vide si pas de proxy sinon saisir ip-proxy:port"
 read ADRESSE_PROXY
 
 
 echo -e "$COLTXT"
-echo -e "Saisissez les hôtes à ignorer par le proxy : $COLSAISIE\c"
-echo "Laisser vide si pas de proxy sinon saisir les hôtes séparés par une virgule (localhost doit obligatoirement en faire partie)"
+echo -e "Saisissez les hôtes à ignorer par le proxy : $COLSAISIE\n"
+echo "Laisser vide si pas de proxy sinon saisir les hôtes à ignorer séparés par une virgule (dans ce cas \"localhost\" doit obligatoirement en faire partie et les caractères spéciaux comme \".\" ou \"*\" sont acceptés)"
 read NO_PROXY
 
 echo -e "$COLINFO"
@@ -87,15 +84,17 @@ echo -e "IP privé :	$ADRESSE_IP_PRIVE"
 echo -e "IP publique :	$ADRESSE_IP_PUBLIQUE"
 echo -e "Proxy:		$ADRESSE_PROXY"
 echo -e "No Proxy:	$NO_PROXY"
+echo -e "$COLCMD"
 
 POURSUIVRE
 
 #Récupération de portainer
 echo -e "$COLPARTIE"
 echo -e "Récupération et configuration de Portainer"
-echo ""
+echo -e "$COLCMD"
+
 if [ -d "/opt/e-comBox_portainer" ]; then
-	echo -e "$COLTXT"
+	echo -e "$COLDEFAUT"
 	echo "Portainer existe et va être remplacé"
 	echo -e "$COLCMD\c"
         cd /opt/e-comBox_portainer
@@ -106,17 +105,16 @@ cd /opt
 git clone https://github.com/siollb/e-comBox_portainer.git
 
 #Configuration de l'adresse IP
-echo -e "$COLTXT"
+echo -e "$COLDEFAUT"
 echo "Mise à jour de /opt/e-comBox_portainer/.env"
-if [ $ADRESSE_IP_PUBLIQUE != "" ] ; then
+echo -e "$COLCMD"
+
+if [ "$ADRESSE_IP_PUBLIQUE" != "" ] ; then
 	URL_UTILE=$ADRESSE_IP_PUBLIQUE
-	else
-		URL_UTILE=$ADRESSE_IP_PRIVE
+	else URL_UTILE=$ADRESSE_IP_PRIVE
 fi
 
 echo -e "$COLCMD\c"
-#echo "DOCKER_IP_LOCALHOST=127.0.0.1" > /opt/e-comBox_portainer/.env
-#echo "DOCKER_IP_HOST=0.0.0.0" > /opt/e-comBox_portainer/.env
 echo "URL_UTILE=$URL_UTILE" >> /opt/e-comBox_portainer/.env
 echo ""
 
@@ -125,34 +123,34 @@ if [ "$ADRESSE_PROXY" != "" ]; then
    if [ ! -d "/etc/systemd/system/docker.service.d" ]; then
 	  mkdir /etc/systemd/system/docker.service.d
    fi
-   echo -e "$COLTXT"
+   echo -e "$COLDEFAUT"
    echo "Ajout des variables d'environnement à systemd (/etc/systemd/system/docker.service.d/http-proxy.conf)"
-   echo ""
+   echo -e "$COLCMD\c"
    echo "[Service]" > /etc/systemd/system/docker.service.d/http-proxy.conf
    echo "Environment=\"HTTP_PROXY=http://$ADRESSE_PROXY" > /etc/systemd/system/docker.service.d/http-proxy.conf
    echo "Environment=\"HTTPS_PROXY=http://$ADRESSE_PROXY" > /etc/systemd/system/docker.service.d/http-proxy.conf
    echo "Environment=\"NO_PROXY=http://$NO_PROXY" > /etc/systemd/system/docker.service.d/http-proxy.conf
    echo ""
-   echo "Redémarrage de Docker"
+   echo -e "Redémarrage de Docker"
    systemctl daemon-reload
    systemctl restart docker   
    else
-	echo -e "$COLTXT"
-	echo "Il n'y a pas de proxy configuré sur le système"
-	echo ""
+	echo -e "$COLINFO"
+	echo "Aucun proxy configuré sur le système"
+	echo -e "$COLCMD"
 fi
 
 # Lancement de Portainer
-echo -e "$COLTXT"
+echo -e "$COLDEFAUT"
 echo "Lancement de portainer"
-echo ""
+echo -e "$COLCMD\c"
 cd /opt/e-comBox_portainer/
 docker-compose up -d
 
 echo -e "$COLINFO"
-echo "Portainer est maintenant accessible à l'URL suivante:"
+echo "Portainer est maintenant accessible à l'URL suivante :"
 echo -e "http://$URL_UTILE:8880"
-echo -e ""
+echo -e "$COLCMD\n"
 
 
 
@@ -161,7 +159,7 @@ echo -e ""
 # Lancement de e-comBox
 echo -e "$COLPARTIE"
 echo "Lancement et configuration de l'environnement de l'application e-comBox"
-echo ""
+echo -e "$COLCMD\c"
 
 if docker ps -a | grep e-combox; then
 	docker rm -f e-combox
@@ -172,14 +170,14 @@ docker run -dit --name e-combox -v ecombox_data:/usr/local/apache2/htdocs/ --res
 # Mise à jour de l'accès à l'API
 
 #echo -e "$COLINFO"
-#echo -e "Lapplication e-comBox sera accessible par défaut via l'adresse IP $ADRESSE_IP"
-#echo -e "Veuillez confirmer cette adresse IP : $COLSAISIE\c"
-#echo "Appuyer sur Entrée si vous confirmez ou saisissez la nouvelle adresse IP"
-#read NEW_IP
-#echo ""
+#echo -e "Lapplication e-comBox sera accessible par défaut via l'URL $URL_UTILE"
+#echo -e "Veuillez confirmer cette adresse URL : $COLSAISIE\c"
+#echo "Appuyer sur Entrée si vous confirmez ou saisissez la nouvelle adresse IP ou le nouveau nom de domaine"
+#read NEW_URL
+#echo "$COLCMD"
 
-#if [ "$NEW_IP" != "" ]; then
-#	ADRESSE_IP=$NEW_IP
+#if [ "$NEW_URL" != "" ]; then
+#	URL_UTILE=$NEW_URL
 #fi
 
 for fichier in /var/lib/docker/volumes/ecombox_data/_data/*.js /var/lib/docker/volumes/ecombox_data/_data/*.js.map
@@ -187,15 +185,21 @@ do
         sed -i -e "s/localhost:8880/$URL_UTILE:8880/g" $fichier
 done
 
-echo -e "$COLINFO"
-echo "L'application e-comBox est maintenant accessible à l'URL suivante:"
-echo -e "http://$URL_UTILE:8888"
-echo -e ""
 
-echo -e "$COLSAISIE"
-echo "tapez sur Entrée pour terminer l'installation"
-echo -e "$COLTXT"
-read FIN
+echo -e "$COLTITRE"
+echo "***************************************************"
+echo "*        FIN DE L'INSTALLATION DE E-COMBOX        *"
+echo "***************************************************"
+
+echo -e "$COLINFO"
+echo "L'application e-comBox est maintenant accessible à l'URL suivante :"
+echo -e "http://$URL_UTILE:8888"
+echo -e "$COLCMD"
+
+#echo -e "$COLSAISIE"
+#echo "Tapez sur Entrée pour terminer l'installation"
+#echo -e "$COLCMD\c"
+#read FIN
 
 
 
--- install_docker_docker-compose.sh
+++ install_docker_docker-compose.sh
@@ -4,7 +4,6 @@
 # Couleurs
 COLTITRE="\033[1;35m"   # Rose
 COLPARTIE="\033[1;34m"  # Bleu
-
 COLTXT="\033[0;37m"     # Gris
 COLCHOIX="\033[1;33m"   # Jaune
 COLDEFAUT="\033[0;33m"  # Brun-jaune
@@ -16,7 +15,7 @@ COLINFO="\033[0;36m"    # Cyan
 clear
 echo -e "$COLTITRE"
 echo "************************************************************"
-echo "*  SCRIPT PERMETTANT D'INSTALLER DOCKER ET DOCKER-COMPOSE  *"
+echo "*         INSTALLATION DE DOCKER ET DOCKER-COMPOSE         *"
 echo "************************************************************"
 
 
@@ -24,6 +23,7 @@ echo "************************************************************"
 # Installation de Docker
 # Utilisation du script officiel fourni par Docker 
 # https://github.com/docker/docker-install pour Docker
+
 echo -e ""
 echo -e "$COLPARTIE"
 echo -e "Installation de Docker"
@@ -43,21 +43,22 @@ echo -e ""
 
 echo -e "$COLCMD"
 
-#Installation des dépendances
+# Installation des dépendances
 apt install -y python-backports.ssl-match-hostname python-cached-property python-docker python-dockerpty python-docopt python-functools32 python-jsonschema python-texttable python-websocket python-yaml git
 
-#Téléchargement de la dernière version du docker-compose
+# Téléchargement de la dernière version du docker-compose
 
-#Récupération de la dernière version dans une variable :
+# Récupération de la dernière version dans une variable :
 COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
 
-#Installation de la dernière version
+# Installation de la dernière version
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
 echo -e ""
 echo -e "$COLINFO"
 echo -e "Docker et Docker-Compose sont installés"
+echo -e "Le script va maintenant procéder à l'installation de e-comBox"
 echo -e ""
 
 echo -e "$COLCMD"
